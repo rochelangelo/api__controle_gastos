c2beced14b130e27035a21ace676115982cb89b0
RochelAngelo <rochelangelo72@gmail.com>
2021-08-17 15:08:27 -0300

    Implementando método para inserção  de dados

--- api/v1/classes/Carteira.php
+++ api/v1/classes/Carteira.php
@@ -1,5 +1,7 @@
 <?php
 
+use JetBrains\PhpStorm\Internal\ReturnTypeContract;
+
 require_once('./common/Environment.php');
 
 Enviroment::load('D:\wamp64\www\api__controle_gastos\api\v1');
@@ -50,11 +52,36 @@ class Carteira
     }
 
     public function inserir(){
+        $data = $_POST;
+        return Carteira::insert($data);
+    }
+
+    public static function insert($data){
         $host = getenv('DB_HOST');
         $user = getenv('DB_USER');
         $pass = getenv('DB_PASS');
         $dbName = getenv('DB_NAME');
 
         $con = new PDO("mysql: host={$host}; dbname={$dbName};", $user, $pass);
+
+        $ndata = Carteira::novaData($data['date']);
+
+        $sql = 'INSERT INTO carteira (description, amount, date) VALUES (:de, :am, :da)';
+        $sql = $con->prepare($sql);
+        $sql->bindValue(':de', $data['description']);
+        $sql->bindValue(':am', intval($data['amount']));
+        $sql->bindValue(':da', date($ndata));
+        $sql->execute();
+
+        if($sql->rowCount() > 0){
+            return 'Dados inseridos com sucesso!';
+        }else{
+            throw new Exception("Falha ao inserir dados!");
+        }
+    }
+
+    public static function novaData(string $data){
+        $novaData = date("Y-m-d", strtotime(str_replace('/', '-', $data)));
+        return $novaData;
     }
 }
